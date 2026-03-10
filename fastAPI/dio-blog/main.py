5d5830c71a372231876d8755349ae6015e8dba22
Taisso Junior Coutinho <taissocout@gmail.com>
2026-03-10 10:36:40 -0300

    Atualização do main.py

--- fastAPI/dio-blog/main.py
+++ fastAPI/dio-blog/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime, timezone
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,6 +12,16 @@ fake_db = [
     {"title": "Criando uma aplicação com Flask", "date": datetime.now(UTC), "published": True},
     {"title": "Criando uma aplicação com Starlett", "date": datetime.now(UTC), "published": True},
 ]
+
+class Post(BaseModel):
+    title: str
+    date: datetime
+    published: bool
+
+@app.post("/posts")
+def create_post(post: Post):
+    fake_db.append(post.dict())
+    return {"message": "Post criado com sucesso!"}
 
 @app.get("/posts")
 def read_posts(skip: int = 0, limit: int = len(fake_db), published: bool = True):
